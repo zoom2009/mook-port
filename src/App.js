febfcad05b0ae9e2886576b95883dc86afeafb5b
afu-hive <afu.sikarinp@hivecorps.com>
2022-11-30 00:35:14 +0700

    init book

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useRef } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNav from './components/MyNav'
 import Favicon from 'react-favicon'
+import Book from './components/book/Book'
 
 const App = () => {
   const normalRef = useRef(null)
@@ -9,7 +10,7 @@ const App = () => {
   const pictureRef = useRef(null)
 
   return (
-    <div>
+    <div style={{ overflowY: 'scroll' }}>
       <Favicon url='favicon.ico' />
       <MyNav
         normalRef={normalRef}
@@ -17,13 +18,15 @@ const App = () => {
         pictureRef={pictureRef}
       />
       <div ref={normalRef} style={{ width: '100%', height: '100vh', backgroundColor: 'pink' }}>
-        
+        <div style={{ paddingTop: 100 }}>
+          <Book />
+        </div>
       </div>
       <div ref={workRef} style={{ width: '100%', height: '100vh', backgroundColor: '#f39c12' }}>
         
       </div>
       <div ref={pictureRef} style={{ width: '100%', height: '100vh', backgroundColor: '#1abc9c' }}>
-        
+
       </div>
     </div>
   )
---
+++ src/components/book/pagesData.js
@@ -0,0 +1,61 @@
+const _0 = (`
+  <div class=\"title\"><h1>Halloween's stories</h1><img src='https://raw.githubusercontent.com/pylnata/livebook/master/public/images/0.png' /></div>
+`)
+
+const _1 = (`
+  <p class="number">1</p>
+  <h3>The Legend of Bride's Head Bridge</h3>
+  <p>There's a bridge near Hanover in Germany called 'Der Kopf der Braut', which means bride's head.  A 15th century legend has it that Reichsgraf von Kesselstatt and his bride Gretchen were approaching the bridge in their horse and carriage when their way was blocked by an elderly crone.</p><p>The Reichsgraf or 'Count' ordered the old lady to get off the bridge instantly and make way for their carriage.  But it was dark, and the old lady had difficulty in herding her sheep off the bridge.</p>
+  <p>Because the old witch was moving none to fast, Reichsgraf von Kesselstatt took his whip and have her a sound thrashing. </p>
+`)
+
+const _2 = (`
+  <p class="number right">2</p>
+  <p> Bleeding, and cowering in a ditch, the old witch put a curse on the carriage.  Consequently when the bridal party eventually crossed the bridge, one of the horses shied and the other reared up.  </p>
+    <p><img src='https://raw.githubusercontent.com/pylnata/livebook/master/public/images/1.jpg' /></p>
+  <p>
+  The upshot was that Gretchen was thrown from the carriage into the river below. It seems certain that she drowned as the river was in torrent and Gretchen was never seen again.
+  </p>
+`)
+
+const _3 = (`
+  <p class="number">3</p>
+  <p>However, it is said by Hanoverian wicca that at Halloween you can see a headless bride standing on rocks in the middle of the river.  Some say she is looking for her lost head, while other say she is looking for her beloved Reichsgraf von Kesselstatt.</p>
+  <p><img src='https://raw.githubusercontent.com/pylnata/livebook/master/public/images/5.png' /></p>
+`)
+
+const _4 = (`
+  <p class="number right">4</p>
+  <h3>Uncle John's Halloween Story</h3>
+  <p>When I was a boy, each year as the nights began to draw in, my uncle John would tell we kids this Halloween story.  It was a tale about a trick that he played in a graveyard.  One night Uncle John spotted his great friend Eddie weaving his way home from the village pub.  As John watched, he saw Eddie open the church's litch gate and take the shortcut through the graveyard.</p>
+<p>There was no doubt that Eddie was the worse for wear, and appeared disoriented, really he should have taken the longer route home via the round ring.  But then he cried out to nobody in particular, 'Where am I?'</p>
+`)
+
+const _5 = (`
+  <p class="number">5</p>
+  <p>John replied instantly, 'Amongst the living'.</p>
+  <p>'Where are you?' cried Eddie'; to which John replied in his most sepulchral voice, 'Amongst the dead'.</p>
+<p>Eddie sobered up instantly, rushed back the way he came, and took the long way around the churchyard.  This time he preferring to go passed the round ring, rather than stay a minute longer amongst the spirits of the gravestones.</p>
+`)
+
+const _6 = (`
+  <p class="number right">6</p>
+  <p>Stories are from <a href="https://www.funny-jokes.com">https://www.funny-jokes.com</a></p>
+`)
+
+const _7 = (`
+  <center style="margin: 60px auto; color: #fff;">&copy; 2019 <a style="color:#fff;" href="http://github.com/pylnata">github.com/pylnata</a></center>
+`)
+
+const pagesData = {
+  0: _0,
+  1: _1,
+  2: _2,
+  3: _3,
+  4: _4,
+  5: _5,
+  6: _6,
+  7: _7,
+}
+
+export default pagesData
